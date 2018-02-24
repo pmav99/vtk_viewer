4c88cf03c7eee3d3b3e7540692df20f84f162433
Panos Mavrogiorgos <pmav99@gmail.com>
2018-02-24 10:18:21 +0200

    [gui][menu] Implement quit + menu boilerplate.

--- main.py
+++ main.py
@@ -18,6 +18,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.file_menu = self.menubar.addMenu("&File")
         self.help_menu = self.menubar.addMenu("&Help")
 
+        # create File Menu actions
+        quit_action = self.create_action("&Quit", "Ctrl+Q", "Quit the app", QtCore.QCoreApplication.instance().quit)
+        self.add_actions(self.file_menu, quit_action)
+
+    def add_actions(self, target, *actions):
+        for action in actions:
+            if action is None:
+                target.addSeparator()
+            else:
+                target.addAction(action)
+
+    def create_action(self, text, shortcut, tip, callback):
+        """ Return a QAction object. """
+        action = QtWidgets.QAction(text, self)
+        action.setShortcut(shortcut)
+        action.setStatusTip(tip)
+        action.triggered.connect(callback)
+        return action
+
 
 if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
