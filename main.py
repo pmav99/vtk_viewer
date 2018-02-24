38043802af26f8eb5906a553f682d62b60f0c728
Panos Mavrogiorgos <pmav99@gmail.com>
2018-02-24 10:09:53 +0200

    [gui] Add menu.

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("VTK Viewer")
+        self.create_menu()
+
+    def create_menu(self):
+        """ Create the menu bar. """
+        # Create the menu Bar.
+        self.menubar = self.menuBar()
+        self.file_menu = self.menubar.addMenu("&File")
+        self.help_menu = self.menubar.addMenu("&Help")
 
 
 if __name__=='__main__':
