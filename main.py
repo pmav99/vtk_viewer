be6630ac7a64373c829c8b62d64edafbd2165287
Panos Mavrogiorgos <pmav99@gmail.com>
2018-02-26 12:01:04 +0200

    [vtk] Add initial implementation for VTKViewer.

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import sys
 
 import vtk
+from vtk.qt import QVTKRenderWindowInteractor
 from qtpy import QtGui
 from qtpy import QtCore
 from qtpy import QtWidgets
+
+
+class VTKViewer(QtWidgets.QFrame):
+    def __init__(self, parent):
+        super(VTKViewer, self).__init__(parent)
+
+        # Make the actual QtWidget a child so that it can be re parented
+        self.interactor = QVTKRenderWindowInteractor.QVTKRenderWindowInteractor(self)
+        self.layout = QtWidgets.QHBoxLayout()
+        self.layout.addWidget(self.interactor)
+        self.layout.setContentsMargins(0,0,0,0)
+        self.setLayout(self.layout)
+
+        # Setup VTK environment
+        self.renderer = vtk.vtkRenderer()
+        self.render_window = self.interactor.GetRenderWindow()
+        self.render_window.AddRenderer(self.renderer)
+
+        self.interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+        self.render_window.SetInteractor(self.interactor)
+        self.renderer.SetBackground(0.8,0.8,0.8)
+
+    def start(self):
+        self.interactor.Initialize()
+        self.interactor.Start()
 
 
 class MyMdiArea(QtWidgets.QMdiArea):
@@ -81,6 +107,13 @@ class MainWindow(QtWidgets.QMainWindow):
         action.triggered.connect(callback)
         return action
 
+    def open_file(self, filename):
+        doc = VTKViewer(self.mdi, filename)
+        self.mdi.addSubWindow(doc)
+        doc.show()
+        doc.start()
+        self.mdi.tileHorizontally()
+
     def on_about(self):
         msg = """Text appearing in the about dialog of our application."""
         QtWidgets.QMessageBox.about(self, "About the demo", msg.strip())
@@ -90,4 +123,6 @@ if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
+    window.open_file("tower.vtk")
+    window.open_file("tower.vtk")
     sys.exit(app.exec_())
